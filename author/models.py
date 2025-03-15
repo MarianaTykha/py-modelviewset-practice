f1a02cbf08b14baafdfc2a541569b9b6cf1b57f7
Мар'яна Тиха <mariana@tikhaya@gmail.com>
2025-03-15 15:33:06 +0200

    Added age field to Author model

--- author/models.py
+++ author/models.py
@@ -3,10 +3,14 @@ from django.db import models
 
 class Author(models.Model):
     first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
-    pseudonym = models.CharField(max_length=64, null=True, blank=True)
+    last_name = models.CharField(max_length=64, db_index=True)
+    pseudonym = models.CharField(max_length=64, null=True, blank=True, unique=True)
     birth_date = models.DateField(null=True, blank=True)
     retired = models.BooleanField(default=False)
+    age = models.IntegerField(null=True, blank=True)
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.get_full_name()
