eab42b89c3941ba1d18dc3579209f548b9eb8623
Мар'яна Тиха <mariana@tikhaya@gmail.com>
2025-03-16 14:08:16 +0200

    Fix flake8 E501: line too long

--- author/models.py
+++ author/models.py
@@ -4,7 +4,12 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64, db_index=True)
-    pseudonym = models.CharField(max_length=64, null=True, blank=True, unique=True)
+    pseudonym = models.CharField(
+        max_length=64,
+        null=True,
+        blank=True,
+        unique=True
+    )
     birth_date = models.DateField(null=True, blank=True)
     retired = models.BooleanField(default=False)
     age = models.IntegerField(null=True, blank=True)
